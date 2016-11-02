eb585d5667d02d4cefdaad702966c1d1954f2612
Marc Dix <marc.dix@aoe.com>
2016-11-02 13:56:52 +0100

    exporting & cleaning up

--- js/jquery.dlmenu.js
+++ js/jquery.dlmenu.js
@@ -8,7 +8,7 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-export default function( $, window, undefined ) {
+module.exports = function( $, window, undefined ) {
 
 	'use strict';
 
